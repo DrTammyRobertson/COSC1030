7c5193c34936735e6f1d814dc5373a1b4ee958f3
Tammy Robertson Callahan <80910439+DrTammyRobertson@users.noreply.github.com>
2024-12-16 05:24:23 +0000

    Ready for grading.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
-// Hello World Programming Project
+// Dr. T
+// 12/15/2024
+// Your First C++ Program
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
